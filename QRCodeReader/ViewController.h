cf7146980169c6deadb6ed69d4fbee932c1eddca
Gabriel Theodoropoulos <gabriel@Gabriel-Theodoropouloss-Mac-mini.local>
2013-11-27 09:03:27 +0200

    Initial Commit

---
+++ QRCodeReader/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  QRCodeReader
+//
+//  Created by Gabriel Theodoropoulos on 27/11/13.
+//  Copyright (c) 2013 Gabriel Theodoropoulos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
