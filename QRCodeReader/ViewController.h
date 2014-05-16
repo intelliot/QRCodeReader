29c2f3c4073294f6c1ba19cc994dbfb12ce0b540
Elliot <elliot@kraken.com>
2014-05-15 18:23:28 -0700

    original code via http://www.appcoda.com/qr-code-ios-programming-tutorial/

--- QRCodeReader/ViewController.h
+++ QRCodeReader/ViewController.h
@@ -7,7 +7,15 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <AVCaptureMetadataOutputObjectsDelegate>
+
+@property (weak, nonatomic) IBOutlet UIView *viewPreview;
+@property (weak, nonatomic) IBOutlet UILabel *lblStatus;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *bbitemStart;
+
+- (IBAction)startStopReading:(id)sender;
+
 
 @end
